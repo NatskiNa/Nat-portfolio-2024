5abdbba3a16253ce6d16b5a364de3f884dac43d7
NatskiNa <143272547+NatskiNa@users.noreply.github.com>
2024-10-01 08:38:42 -0700

    Crested ProjectData.js

---
+++ src/components/pages/ProjectData.js
@@ -0,0 +1,53 @@
+const ProjectData = [
+  {
+    id: 1,
+    jobTitle: 'Junior Web Developer',
+    projectTitle: 'Ahmed Tech Academy',
+    introduction:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel fermentum nunc neque vel dui. Nullam et purus sit amet sem fermentum ultrices.',
+    skills: ['HTML5', 'CSS3', 'JavaScript'],
+    images: ['image1', 'image2'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel fermentum nunc neque vel dui. Nullam et purus sit amet sem fermentum ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel fermentum nunc neque vel dui. Nullam et purus sit amet sem fermentum ultrices.',
+    url: 'https://www.ahmedtechacademy.com/',
+  },
+  {
+    id: 2,
+    jobTitle: 'Personal Project',
+    projectTitle: 'Project 2',
+    introduction:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel fermentum nunc neque vel dui. Nullam et purus sit amet sem fermentum ultrices.',
+    skills: ['HTML5', 'CSS3', 'JavaScript'],
+    images: ['image1', 'image2'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel fermentum nunc neque vel dui. Nullam et purus sit amet sem fermentum ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel fermentum nunc neque vel dui. Nullam et purus sit amet sem fermentum ultrices.',
+    url: '',
+  },
+
+  {
+    id: 3,
+    jobTitle: 'Personal Project',
+    projectTitle: 'Project 3',
+    introduction:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel fermentum nunc neque vel dui. Nullam et purus sit amet sem fermentum ultrices.',
+    skills: ['HTML5', 'CSS3', 'JavaScript'],
+    images: ['image1', 'image2'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel fermentum nunc neque vel dui. Nullam et purus sit amet sem fermentum ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel fermentum nunc neque vel dui. Nullam et purus sit amet sem fermentum ultrices.',
+    url: '',
+  },
+  {
+    id: 4,
+    jobTitle: 'Personal Project',
+    projectTitle: 'Project 4',
+    introduction:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel fermentum nunc neque vel dui. Nullam et purus sit amet sem fermentum ultrices.',
+    skills: ['HTML5', 'CSS3', 'JavaScript'],
+    images: ['image1', 'image2'],
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel fermentum nunc neque vel dui. Nullam et purus sit amet sem fermentum ultrices. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel fermentum nunc neque vel dui. Nullam et purus sit amet sem fermentum ultrices.',
+    url: '',
+  },
+];
+
+export default ProjectData;
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -3,8 +3,36 @@ import './styles/Projects.css';
 
 const Projects = () => {
   return (
-    <div id="projects" className="projects-section">
-      Projects
+    <div id="projects" className="page-section">
+      <h1 className="section-title">PROJECTS</h1>
+      <div className="section-container">
+        <div className="project">
+          <p className="job-title">Junior Web Developer</p>
+          <h2 className="content-title">Ahmed Tech Academy</h2>
+          <div className="project-content">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              pulvinar, mi et tincidunt bibendum, magna dui tincidunt ex, vel
+              fermentum nunc neque vel dui. Nullam et purus sit amet sem
+              fermentum ultrices.
+            </p>
+            <div className="skills-content">
+              <div className="skill-item">HTML5</div>
+              <div className="skill-item">CSS3</div>
+              <div className="skill-item">JavaScript</div>
+            </div>
+            <div className="button-container">
+              <a href="/NN_CV.pdf" target="_blank" rel="noopener noreferrer">
+                <button>VIEW PROJECT</button>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="project-images">
+          <div className="image-container">images</div>
+        </div>
+      </div>
     </div>
   );
 };
